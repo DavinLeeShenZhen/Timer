f12ee9f80e2b95e329808537e68ac024d08a148e
panda <pmpanda@qq.com>
2017-08-10 10:02:35 +0800

    Initial Commit

---
+++ Timer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Timer
+//
+//  Created by lidapeng on 2017/8/10.
+//  Copyright © 2017年 lidapeng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Timer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Timer
+//
+//  Created by lidapeng on 2017/8/10.
+//  Copyright © 2017年 lidapeng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
